58bd8095a26b1f9c1e086db17f6ddefce7f001a1
Yurii Soroka <urasor04@gmail.com>
2024-08-28 15:49:33 +0300

    Provided data parsing with Buffer.concat(body).toString(), added the correct writeFileSync logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
@@ -12,7 +13,16 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+          body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
