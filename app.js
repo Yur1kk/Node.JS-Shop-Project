159d83480c9ece9f68386ec9e26629793f8be63f
Yurii Soroka <urasor04@gmail.com>
2024-09-01 01:57:33 +0300

    Updated our server with body-parser and some express features

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(routes);
+app.use();
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+   
+   res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
