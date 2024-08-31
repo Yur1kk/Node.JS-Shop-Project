c844cc47d928ac1902be17b90713f854c3f4058e
Yurii Soroka <urasor04@gmail.com>
2024-09-01 02:48:48 +0300

    Changed console.log text, added statuscode

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    console.log('In add-product middleware!');
+    res.status(200).send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
 router.post('/product', (req, res, next) => {
-   
-   res.redirect('/');
+   console.log(req.body);
+   res.status(201).redirect('/');
 });
 
 module.exports = router;
