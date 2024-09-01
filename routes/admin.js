ec36c80844032c16e91350bf658fa9be604e04dc
Yurii Soroka <urasor04@gmail.com>
2024-09-01 23:58:24 +0300

    Updated routes export

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    console.log('In add-product middleware!');
     res.status(200).sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-   console.log(req.body);
+   products.push({title: req.body.title});
    res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
