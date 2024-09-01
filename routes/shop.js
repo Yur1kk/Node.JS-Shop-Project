b2cdca49f30cc86b96de46237433046b09cec181
Yurii Soroka <urasor04@gmail.com>
2024-09-02 02:57:34 +0300

    Used Model View Controll approach, replaced logic to controllers folder

--- routes/shop.js
+++ routes/shop.js
@@ -4,19 +4,9 @@ const express = require('express');
 
 
 const router = express.Router();
-const adminData = require('./admin');
+const productsController = require('../controllers/products')
 
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.status(200).render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
