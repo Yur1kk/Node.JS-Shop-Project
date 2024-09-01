fbace878855230e29d34384463180ab7a90d66cb
Yurii Soroka <urasor04@gmail.com>
2024-09-01 03:51:34 +0300

    Provided path to template via PATH module

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log('In main middleware!');
-    res.status(200).send('<h1>Hello from Express!</h1>');
+    res.status(200).sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
