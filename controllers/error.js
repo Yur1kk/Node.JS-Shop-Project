66865ec7810e342882813707c9ae4aa5c8ee6e4e
Yurii Soroka <urasor04@gmail.com>
2024-09-03 02:41:38 +0300

    Updated error controller

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.getPageNotFound = (req, res, next) => {
-    res.status(404).render('pageNotFound', {pageTitle: 'Page Not Found'});
+    res.status(404).render('pageNotFound', {pageTitle: 'Page Not Found', path: '/pageNotFound'});
 };
