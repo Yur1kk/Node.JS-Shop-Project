ab4e57aea8b8b2c00c0ca7c4eaf13d65c068026a
Yurii Soroka <urasor04@gmail.com>
2024-09-27 20:14:31 +0300

    Added getEditProduct controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.status(200).render("admin/add-product", {
+    res.status(200).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true,
     });
   };
   
