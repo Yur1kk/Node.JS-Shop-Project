3ba7b1f177c3fbf6f63d7a2e76ad32d938c0ae25
Yurii Soroka <urasor04@gmail.com>
2024-10-14 16:43:03 +0300

    Minor code changes

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -44,7 +53,7 @@ sequelize
     return user;
 })
 .then(user => {
-    console.log(user);
+    // console.log(user);
     app.listen(3000);
     })
 .catch(err => {
