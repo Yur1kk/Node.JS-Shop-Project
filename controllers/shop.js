fd41d14838a6147fe3747b333194f7026825b346
Yurii Soroka <urasor04@gmail.com>
2024-10-15 09:19:20 +0300

    Replaced shop controller function syntax with sequelize syntax

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,30 +39,27 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart.getProducts()
+    .then(products => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: cartProducts,
-        totalPrice: cart.totalPrice
+        products: products,
       });
-    });
+    })
+    .catch(err => console.log(err));
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
@@ -70,7 +67,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
   });
