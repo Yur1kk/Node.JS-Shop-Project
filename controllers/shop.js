5de7b83c463b9aace0d67a5b60300a9a47cd1f40
Yurii Soroka <urasor04@gmail.com>
2024-10-15 17:47:28 +0300

    Fixed and modified postCart and postCartDelete methods

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,17 +59,54 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts({where: {id: prodId}});
+  })
+  .then(products => {
+    let product;
+    if (products.length > 0) {
+      product = products[0];
+    }
+    let newQuantity = 1;
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return fetchedCart.addProduct(product, {
+          through: { quantity: newQuantity }
+        });
+      }
+      return Product.findByPk(prodId)
+      .then(product => {
+        return fetchedCart.addProduct(product, {through: { quantity: newQuantity}});
+      })
+      .catch(err => console.log(err));
+  })
+  .then((result) => {
+    res.redirect('/cart')
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart.getProducts({where: {id: prodId}});
+  })
+  .then(products => {
+    const product = products[0];
+    return product.cartItem.destroy();
+  })
+  .then(result => {
     res.redirect('/cart');
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
